b161271ab2188309270d7186f82ff400629a3a2f
David Kuda <david.kuda@pm.me>
2021-05-08 19:27:57 +0200

    style: Use JS standard style

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import Link from 'next/Link'
 
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return (
     <div>
       <nav>
-      <ul>
-        <li><Link href="/"><a>Home</a></Link></li>
-        <li><Link href="/about"><a>About</a></Link></li>
-      </ul>
+        <ul>
+          <li><Link href='/'><a>Home</a></Link></li>
+          <li><Link href='/about'><a>About</a></Link></li>
+        </ul>
       </nav>
       <Component {...pageProps} />
     </div>
