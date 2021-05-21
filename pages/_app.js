b43076a7fcbffb84867e6323439971e577d2838b
David Kuda <david.kuda@pm.me>
2021-05-21 21:59:01 +0200

    Setup initial layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,18 @@ import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link href='/'><a>Home</a></Link></li>
-          <li><Link href='/about'><a>About</a></Link></li>
-        </ul>
-      </nav>
+    <div className='mx-auto w-3/5 my-20'>
+      <header>
+        <h1 className='text-4xl font-bold text-center'>
+          kuda.ai | code. guitar. life.
+        </h1>
+        <nav className='my-4'>
+          <ul className='flex flex-row justify-center space-x-4'>
+            <li><Link href='/'><a>Home</a></Link></li>
+            <li><Link href='/about'><a>About</a></Link></li>
+          </ul>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </div>
   )
