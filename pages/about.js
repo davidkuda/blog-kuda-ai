35ece4a24908c444a0477a3c7a877a8808ccd297
David Kuda <david.kuda@pm.me>
2022-02-13 00:02:37 +0100

    Add class "simple-list" to ul on about page

--- pages/about.js
+++ pages/about.js
@@ -16,14 +16,14 @@ export default function About () {
         <p>I have many interests and am very curious and open-minded by nature.</p>
 
         <h2>The topics I will probably cover in this blog:</h2>
-        <ul>
+        <ul className='simple-list'>
           <li><b>Code.</b> Software Engineering has become such a big thing in my life. It has been an incredible journey and I am very grateful that I can do what I do. Sometimes I find things that I do so interesting that I want to write about them and share them with the world. Sometimes I figure out things that might be useful for other people too. A recent example could be: "How can you authenticate to AWS ECR to register your docker images from GitLab CI?" Other ideas are reviewing online courses or pondering upon the technologies I use for this blog.</li>
           <li><b>Guitar.</b> I love playing the guitar and jam around. When you play an instrument you might sometimes have aha-moments. Thinking about these aha-moments is meaningful to me and I would like to share some of these every now and then.</li>
           <li><b>Life.</b> What does life mean? Why are we here? What is really meaningful? I have been thinking about these things since a childhood, and perhaps I might write about these things as well.</li>
         </ul>
         <p>I am never exactly sure why of all people you should read me / listen to me. Who am I that I could tell you something about these things? I don't know. I want to avoid meaninglessness, redundancy, irrelevance and I truly want to bring value to those who take the moment to listen. I regard this blog as an experiment, let's see where it leads.</p>
         <h2>Why I am starting this blog:</h2>
-        <ul>
+        <ul className='simple-list'>
           <li>Bring value to the world, or at least to those on the world who share the same interests as I do.</li>
           <li>To have a space to create things, as I like creating things.</li>
           <li>Get better at web development, experiment and tinker with technology.</li>
