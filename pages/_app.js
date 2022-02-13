5694750ddc326811f2d7616a4edf7614a31d38d7
David Kuda <david.kuda@pm.me>
2022-02-13 22:28:33 +0100

    Remove link to music page

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,13 @@ function MyApp ({ Component, pageProps }) {
   return (
     <div className='mx-auto w-3/5 my-20'>
       <header>
-        <h1 className='text-4xl font-bold text-center'>
+        <h1 className='text-4xl font-bold text-center gradient-text'>
           kuda.ai | code. guitar. life.
         </h1>
         <nav className='my-4'>
-          <ul className='flex flex-row justify-center space-x-4'>
+          <ul className='flex flex-row justify-center space-x-4 gradient-text'>
             <li><Link href='/'><a>Home</a></Link></li>
             <li><Link href='/about'><a>About</a></Link></li>
-            <li><Link href='/music'><a>Music</a></Link></li>
           </ul>
         </nav>
       </header>
