148c92c855f1c9117721426568adee5dd899808b
David Kuda <david.kuda@pm.me>
2022-02-13 22:52:41 +0100

    style: Apply new code formatter

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,32 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp ({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <div className='mx-auto w-3/5 my-20'>
+    <div className="mx-auto w-3/5 my-20">
       <header>
-        <h1 className='text-4xl font-bold text-center gradient-text'>
+        <h1 className="text-4xl font-bold text-center gradient-text">
           kuda.ai | code. guitar. life.
         </h1>
-        <nav className='my-4'>
-          <ul className='flex flex-row justify-center space-x-4 gradient-text'>
-            <li><Link href='/'><a>Home</a></Link></li>
-            <li><Link href='/about'><a>About</a></Link></li>
+        <nav className="my-4">
+          <ul className="flex flex-row justify-center space-x-4 gradient-text">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
           </ul>
         </nav>
       </header>
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
