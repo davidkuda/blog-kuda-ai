44fbaa860fae342b1956915fdf9c2c20c907cad4
David Kuda <david.kuda@pm.me>
2022-02-26 11:43:34 +0100

    Remove "purge" fron tailwind.config (it is deprecated)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,6 @@ module.exports = {
     // For the best performance and to avoid false positives,
     // be as specific as possible with your content configuration.
   ],
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
