4458b09bce19906b37fd5feac1db8ab930ab638c
David Kuda <david.kuda@pm.me>
2022-02-26 11:44:11 +0100

    Set darkMode to media

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // For the best performance and to avoid false positives,
     // be as specific as possible with your content configuration.
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media",
   theme: {
     extend: {},
   },
