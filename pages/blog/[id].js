3d3e8814bc1d1081036468bede5e0680dd0f5902
David Kuda <david.kuda@pm.me>
2022-03-02 23:16:02 +0100

    Apply code formatter

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -13,19 +13,21 @@ export default function Post({ postData }) {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className="flex flex-col md:items-center">
-        <main className="prose prose-sm md:prose-base">
-          <div className="mb-4">
-            <h2 className="md:text-3xl text-xl font-bold">{postData.title}</h2>
+          <main className="prose prose-sm md:prose-base">
+            <div className="mb-4">
+              <h2 className="md:text-3xl text-xl font-bold">
+                {postData.title}
+              </h2>
 
-            <div className="text-gray-600 text-xs">
-              {format(parseISO(postData.creationDate), "d. MMMM uuu")}
+              <div className="text-gray-600 text-xs">
+                {format(parseISO(postData.creationDate), "d. MMMM uuu")}
+              </div>
             </div>
-          </div>
 
-          <div>
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-          </div>
-        </main>
+            <div>
+              <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </div>
+          </main>
         </div>
       </div>
     </>
@@ -43,7 +45,7 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
   const allPosts = await getAllPosts();
-  const paths = allPosts.map(p => `/blog/${p.fields.id}`);
+  const paths = allPosts.map((p) => `/blog/${p.fields.id}`);
   return {
     paths: paths,
     fallback: true,
