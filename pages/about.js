75996ced2ceb8c4ba9c503a5ef14988d1eafae05
David Kuda <david.kuda@pm.me>
2022-03-02 23:41:01 +0100

    Replace img

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function About() {
+  var img = "https://images.ctfassets.net/pedj0c0bs6fa/7dr3gvDG0rlRZofUmDvJWx/00d13a6a2a23a749f46fd13cd7d4e057/DKU.jpg?w=800";
   return (
     <div>
       <Head>
@@ -15,14 +16,14 @@ export default function About() {
         <meta property="og:url" content="https://www.kuda.ai/about" />
         <meta
           property="og:image"
-          content="https://images.ctfassets.net/pedj0c0bs6fa/qwPcHTncKrThonbe8Pawy/03ce84edcd7470ca8d622a404e7d2e8a/dave.jpg?w=800"
+          content={img}
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="flex flex-col md:items-center">
         <main className="prose text-sm md:text-base">
-          <img src="https://images.ctfassets.net/pedj0c0bs6fa/qwPcHTncKrThonbe8Pawy/03ce84edcd7470ca8d622a404e7d2e8a/dave.jpg?w=800" />
+          <img src={img} />
           <p>Hello ! Thank you so much for coming here !</p>
           <p>
             My name is David ( some call me DataDave ^^ ), I am a Data Engineer
