08c16bf3f33b000222854f614a6d60b209270fb1
David Kuda <david.kuda@pm.me>
2022-03-16 16:47:11 +0100

    Add img from contentful to about page

--- pages/about.js
+++ pages/about.js
@@ -5,13 +5,11 @@ import { getMarkdownContent } from "../lib/contentful_api";
 import Footer from "../components/Footer";
 
 export default function About(props) {
-  var img = "https://images.ctfassets.net/pedj0c0bs6fa/7dr3gvDG0rlRZofUmDvJWx/00d13a6a2a23a749f46fd13cd7d4e057/DKU.jpg?w=800";
   return (
     <div>
       <Headers title="About | kuda.ai" url_endpoint="about" />
       <div className="flex flex-col md:items-center">
         <main className="prose text-sm md:text-base">
-          <img src={img} />
           <div dangerouslySetInnerHTML={{ __html: props.aboutPageContent }} />
       </main>
       </div>
