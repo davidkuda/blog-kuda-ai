e07fdf7b9e34e6f126bd315eb3850e6537a335a7
David Kuda <david@kuda.ai>
2022-09-22 23:26:28 +0200

    change default preview image

--- components/Headers.js
+++ components/Headers.js
@@ -13,7 +13,8 @@ export default function Headers(props) {
 
   if (headers.previewImage === null) {
     headers.previewImage =
-      "https://images.ctfassets.net/pedj0c0bs6fa/7dr3gvDG0rlRZofUmDvJWx/00d13a6a2a23a749f46fd13cd7d4e057/DKU.jpg?w=800";
+      "https://images.ctfassets.net/pedj0c0bs6fa/54o6IJpJFmV2D57ZHeyNKS/c4f97ba8b50eea3759c9eac4e52e2ed0/davidkuda_profile_picture.jpeg"
+      + "?w=800";
   }
 
   return (
