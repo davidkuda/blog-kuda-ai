5defde8c4c0d6c6b5a0ed41ae17bf7d15fce4519
davidkuda <david@kuda.ai>
2023-03-04 20:53:12 +0100

    feat: implement a login page

---
+++ components/LoginForm.js
@@ -0,0 +1,53 @@
+export default function LoginForm(props) {
+  return (
+    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="max-w-md mx-auto">
+          <div className="divide-y divide-gray-200">
+            <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              <form onSubmit={props.submitHandler}>
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="email"
+                    name="email"
+                    type="email"
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Email Address"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Email Address
+                  </label>
+                </div>
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Password"
+                  />
+                  <label
+                    htmlFor="password"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="relative">
+                  <button className="gradient-fill text-white rounded-md px-2 py-1 mt-4 ">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/login.js
@@ -0,0 +1,19 @@
+import Headers from "../components/Headers";
+import LoginForm from "../components/LoginForm";
+import Footer from "../components/Footer";
+
+export default function Login() {
+  var headers = {};
+  return (
+    <div>
+      <Headers headers={headers} />
+      <LoginForm submitHandler={handleLogin} />
+      <Footer />
+    </div>
+  );
+}
+
+function handleLogin(event) {
+  event.preventDefault();
+  console.log(event.target)
+}
