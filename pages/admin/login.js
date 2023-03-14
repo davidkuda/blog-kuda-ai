60ff606ea32eeab36ccadc86273a4fd1801d597d
davidkuda <david@kuda.ai>
2023-03-14 21:33:45 +0100

    rfr: update login page: use new token obj

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -2,18 +2,16 @@ import Headers from "../../components/Headers";
 import LoginForm from "../../components/LoginForm";
 import Footer from "../../components/Footer";
 
-export default function Login() {
+
+export default function Login(props) {
   var headers = {};
+  var loggedIn = <p className="text-center my-20">You are logged in :)</p>
+
   return (
     <div>
       <Headers headers={headers} />
-      <LoginForm submitHandler={handleLogin} />
+      {props.token ? loggedIn : <LoginForm setToken={props.setToken} />}
       <Footer />
     </div>
   );
-}
-
-function handleLogin(event) {
-  event.preventDefault();
-  console.log(event.target)
 }
