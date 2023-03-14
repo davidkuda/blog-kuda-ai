be01dc0715e62647fbf2b04e963a55ae089e86c1
davidkuda <david@kuda.ai>
2023-03-14 21:29:02 +0100

    feat: pass global state "token"

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
-import Link from "next/link";
+import { useState } from "react"
+
 import NavBar from "../components/NavBar";
 
 import "../styles/globals.css";
 // import "../styles/prism.css";
 
 function App({ Component, pageProps }) {
+  const [token, setToken] = useState()
   return (
     <div className="mx-auto md:w-3/5 w-10/12 my-20">
       <NavBar />
-      <Component {...pageProps} />
+      <Component token={token} setToken={setToken} {...pageProps} />
     </div>
   );
 }
