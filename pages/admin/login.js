3dd4225de841b4b19a842688cd9e11f9bdf7fb1f
davidkuda <david@kuda.ai>
2023-04-10 19:03:28 +0200

    refactor: extract "hasActiveSession" to own file

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -4,12 +4,14 @@ import Headers from "../../components/Headers";
 import LoginForm from "../../components/LoginForm";
 import Footer from "../../components/Footer";
 
+import { hasActiveSession } from "../../lib/lyricsapi";
+
 export default function Login() {
   let [failedAttempt, setFailedAttempt] = useState();
   let [isLoggedIn, setIsLoggedIn] = useState(false);
   let [userName, setUserName] = useState();
 
-  checkLoginState(setIsLoggedIn, setUserName);
+  checkSession(setIsLoggedIn, setUserName);
 
   var headers = {};
 
@@ -61,23 +63,11 @@ export default function Login() {
   );
 }
 
-async function checkLoginState(setIsLoggedIn, setUserName) {
-  // check if there is an active session based on cookie
-  var lyricsAPIURL = process.env.NEXT_PUBLIC_LYRICSAPI_BASE_URL;
-  var res = await fetch(lyricsAPIURL + "/session", {
-    method: "GET",
-    credentials: "include",
-    cache: "no-cache",
-  });
-
-  if (res.status == 401) {
-    setIsLoggedIn(false);
-    return;
-  }
-
-  if (res.status == 200) {
-    setIsLoggedIn(true);
-    let sessionData = await res.json();
-    setUserName(sessionData.session);
+async function checkSession(l, n) {
+  let [hasSession, sessionData] = await hasActiveSession();
+  console.log(hasSession, sessionData);
+  l(hasSession);
+  if (hasSession) {
+    n(sessionData.session);
   }
 }
