03f9b5372547b5926dff6dfc27468d89f9c64bd8
davidkuda <david@kuda.ai>
2023-04-11 22:07:05 +0200

    reduce width

--- pages/covers.js
+++ pages/covers.js
@@ -19,31 +19,33 @@ export default function Covers(props) {
   return (
     <div>
       <Headers headers={{ title: "kuda.ai | Guitar Songs I like to play" }} />
-      <main className="space-y-4 mt-16">
-        <p>
-          {" "}
-          I learned a lot by playing songs and figuring out their chords by by
-          ear. Here I publish my transcriptions.{" "}
-        </p>
-        <p>
-          Occasionally, I find myself with a guitar in front of a group or among
-          other musicians. Then we think about songs. This list is going to help
-          future me in such situations. :)
-        </p>
-        <ul className="blog-posts-container flex flex-row flex-wrap justify-center items-center">
-          {props.songs.map((song) => (
-            <li className="w-2/3 mx-2 my-3" key={song.id}>
-              <Link
-                href={"covers/" + song.id}
-                className="cursor-pointer font-medium underline"
-              >
-                {song.artist}: {song.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      {isLoggedIn ? <NewSongForm token={props.token} /> : ""}
+      <div className="flex flex-col md:items-center">
+        <main className="prose text-sm md:text-base">
+          <p>
+            {" "}
+            I learned a lot by playing songs and figuring out their chords by by
+            ear. Here I publish my transcriptions.{" "}
+          </p>
+          <p>
+            Occasionally, I find myself with a guitar in front of a group or among
+            other musicians. Then we think about songs. This list is going to help
+            future me in such situations. :)
+          </p>
+          <ul className="blog-posts-container flex flex-row flex-wrap justify-center items-center">
+            {props.songs.map((song) => (
+              <li className="w-2/3 mx-2 my-3" key={song.id}>
+                <Link
+                  href={"covers/" + song.id}
+                  className="cursor-pointer font-medium underline"
+                >
+                  {song.artist}: {song.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {isLoggedIn ? <NewSongForm token={props.token} /> : ""}
+        </main>
+      </div>
       <Footer />
     </div>
   );
