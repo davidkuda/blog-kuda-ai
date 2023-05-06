d71d7f359932753d8394b5562a90d76ab7d8cfee
davidkuda <david@kuda.ai>
2023-05-06 22:46:36 +0200

    remove token and setToken state from previous auth flow

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
-import { useState } from "react"
-
 import NavBar from "../components/NavBar";
 
 import "../styles/globals.css";
-// import "../styles/prism.css";
 
 function App({ Component, pageProps }) {
-  const [token, setToken] = useState()
   return (
     <div className="mx-auto md:w-3/5 w-10/12 my-20">
       <NavBar />
-      <Component token={token} setToken={setToken} {...pageProps} />
+      <Component {...pageProps} />
     </div>
   );
 }
