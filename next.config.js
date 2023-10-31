1f9f722ba2bb32790fd4abb30566259fef2a697f
David O. Kuda <david@kuda.ai>
2023-10-31 13:11:51 +0100

    forward /name-linkedin

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/forward/name-linkedin",
+        destination: "https://www.linkedin.com/pulse/why-i-have-decided-change-my-first-name-david-ozzie-kuda/",
+        permanent: true,
+      },
+    ]
+  },
+}
